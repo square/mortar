fa1054ce9da748c333fe107a64fdacfc0e16ca3b
Ray Ryan <ray@squareup.com>
2013-12-12 13:15:24 -0800

    Remove MortarContextWrapper from the public API

--- mortar/src/main/java/mortar/MortarContextWrapper.java
+++ mortar/src/main/java/mortar/MortarContextWrapper.java
@@ -19,7 +19,7 @@ import android.content.Context;
 import android.content.ContextWrapper;
 import android.view.LayoutInflater;
 
-public class MortarContextWrapper extends ContextWrapper implements HasMortarScope {
+class MortarContextWrapper extends ContextWrapper implements HasMortarScope {
   private final MortarScope scope;
 
   private LayoutInflater inflater;
