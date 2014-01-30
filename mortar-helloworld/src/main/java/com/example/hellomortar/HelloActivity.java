d806aa933443964060f0a179db481c21a07cbef9
Ray Ryan <ray@squareup.com>
2014-01-30 11:22:22 -0800

    Hello Mortar

---
+++ mortar-helloworld/src/main/java/com/example/hellomortar/HelloActivity.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2014 Square Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.hellomortar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import mortar.Mortar;
+import mortar.MortarActivityScope;
+import mortar.MortarContext;
+import mortar.MortarScope;
+
+public class HelloActivity extends Activity implements MortarContext {
+  private MortarActivityScope activityScope;
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    MortarScope parentScope = ((HelloApplication) getApplication()).getRootScope();
+    activityScope = Mortar.requireActivityScope(parentScope, new Main());
+    Mortar.inject(this, this);
+
+    activityScope.onCreate(savedInstanceState);
+    setContentView(R.layout.main_view);
+  }
+
+  @Override protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    activityScope.onSaveInstanceState(outState);
+  }
+
+  @Override protected void onDestroy() {
+    super.onDestroy();
+
+    if (isFinishing() && activityScope != null) {
+      activityScope.destroy();
+      activityScope = null;
+    }
+  }
+
+  @Override public MortarScope getMortarScope() {
+    return activityScope;
+  }
+}
---
+++ mortar-helloworld/src/main/java/com/example/hellomortar/MainView.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2014 Square Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.hellomortar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import javax.inject.Inject;
+import mortar.Mortar;
+
+public class MainView extends LinearLayout {
+  @Inject Main.Presenter presenter;
+
+  private TextView textView;
+
+  public MainView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    Mortar.inject(context, this);
+  }
+
+  @Override protected void onFinishInflate() {
+    super.onFinishInflate();
+    textView = (TextView) findViewById(R.id.text);
+    presenter.takeView(this);
+  }
+
+  @Override protected void onDetachedFromWindow() {
+    super.onDetachedFromWindow();
+    presenter.dropView(this);
+  }
+
+  public void show(CharSequence stuff) {
+    textView.setText(stuff);
+  }
+}
