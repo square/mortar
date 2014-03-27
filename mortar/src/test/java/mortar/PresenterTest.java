2c93330857d937eea1cb53cbd75227974fb72bc1
Ray Ryan <ray@squareup.com>
2014-03-27 16:10:22 -0700

    Debounces Presenter#onLoad, makes dropView more reliable.
    Addresses https://github.com/square/mortar/issues/59.

--- mortar/src/test/java/mortar/PresenterTest.java
+++ mortar/src/test/java/mortar/PresenterTest.java
@@ -83,17 +83,28 @@ public class PresenterTest {
       childOne.takeView(getView());
       childTwo.takeView(getView());
     }
+
+    @Override public void dropView(SomeView view) {
+      childTwo.dropView(view);
+      childOne.dropView(view);
+      super.dropView(view);
+    }
   }
 
   @Test public void childPresentersGetTheirOwnBundles() {
     scope.onCreate(null);
 
     ParentPresenter presenter = new ParentPresenter();
-    presenter.takeView(new SomeView());
+    SomeView view = new SomeView();
+
+    presenter.takeView(view);
 
     Bundle bundle = new Bundle();
     scope.onSaveInstanceState(bundle);
+    presenter.dropView(view);
+
     scope.onCreate(bundle);
+    presenter.takeView(view);
 
     /**
      * Assertions in {@link ChildPresenter#onLoad(android.os.Bundle)} are the real test,
@@ -102,5 +113,77 @@ public class PresenterTest {
 
     assertThat(presenter.childOne.loaded).isTrue();
     assertThat(presenter.childTwo.loaded).isTrue();
+  }
+
+  class SimplePresenter extends Presenter<SomeView> {
+    boolean loaded;
+    Object droppedView;
+
+    @Override protected MortarScope extractScope(SomeView view) {
+      return scope;
+    }
+
+    @Override protected void onLoad(Bundle savedInstanceState) {
+      loaded = true;
+    }
+
+    @Override public void dropView(SomeView view) {
+      droppedView = view;
+      super.dropView(view);
+    }
+  }
+
+  /**  https://github.com/square/mortar/issues/59 */
+  @Test public void onLoadOnlyOncePerView() {
+    SimplePresenter presenter = new SimplePresenter();
+    SomeView view = new SomeView();
+
+    presenter.takeView(view);
+    assertThat(presenter.loaded).isTrue();
+
+    presenter.loaded = false;
+    scope.onCreate(null);
+    assertThat(presenter.loaded).isFalse();
+  }
+
+  @Test public void newViewNewLoad() {
+    SimplePresenter presenter = new SimplePresenter();
+    SomeView viewOne = new SomeView();
+
+    presenter.takeView(viewOne);
+    assertThat(presenter.loaded).isTrue();
+
+    presenter.loaded = false;
+    SomeView viewTwo = new SomeView();
+    presenter.takeView(viewTwo);
+    assertThat(presenter.loaded).isTrue();
+  }
+
+  @Test public void dropRetakeReload() {
+    SimplePresenter presenter = new SimplePresenter();
+    SomeView view = new SomeView();
+
+    presenter.takeView(view);
+    assertThat(presenter.loaded).isTrue();
+
+    presenter.dropView(view);
+
+    presenter.loaded = false;
+    presenter.takeView(view);
+    assertThat(presenter.loaded).isTrue();
+  }
+
+  /**
+   * When takeView clobbers an existing view, dropView should be called. (We could
+   * drop this requirement if dropView were final, see https://github.com/square/mortar/issues/52)
+   */
+  @Test public void autoDropCallsDrop() {
+    SimplePresenter presenter = new SimplePresenter();
+    SomeView viewOne = new SomeView();
+    SomeView viewTwo = new SomeView();
+
+    presenter.takeView(viewOne);
+    presenter.takeView(viewTwo);
+    assertThat(presenter.droppedView).isSameAs(viewOne);
   }
 }
