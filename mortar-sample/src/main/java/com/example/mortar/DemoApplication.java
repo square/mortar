8754f91c40451b4e9b28072a7d47a713e09bed5f
Ray Ryan <ray@squareup.com>
2014-04-04 17:50:35 -0700

    Snip naive RxJava brain damage in DemoApplication

--- mortar-sample/src/main/java/com/example/mortar/DemoApplication.java
+++ mortar-sample/src/main/java/com/example/mortar/DemoApplication.java
@@ -20,21 +20,12 @@ import com.example.mortar.core.ApplicationModule;
 import dagger.ObjectGraph;
 import mortar.Mortar;
 import mortar.MortarScope;
-import rx.plugins.RxJavaErrorHandler;
-import rx.plugins.RxJavaPlugins;
 
 public class DemoApplication extends Application {
   private MortarScope rootScope;
 
   @Override public void onCreate() {
     super.onCreate();
-
-    // So that exceptions thrown in RxJava onError methods don't have their stack traces swallowed.
-    RxJavaPlugins.getInstance().registerErrorHandler(new RxJavaErrorHandler() {
-      @Override public void handleError(Throwable e) {
-        throw new RuntimeException(e);
-      }
-    });
 
     rootScope =
         Mortar.createRootScope(BuildConfig.DEBUG, ObjectGraph.create(new ApplicationModule()));
